f8f3188157ac603e7ae8e32ea0879849df8694f6
Amit Hens <hens.is.here@gmail.com>
2025-02-09 03:45:34 +0530

    TokenManager class added

---
+++ src/test/java/com/spotifyapi/api/TokenManager.java
@@ -0,0 +1,57 @@
+package com.spotifyapi.api;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.time.Instant;
+import java.util.HashMap;
+
+import static com.spotifyapi.api.SpecBuilder.getResponseSpecification;
+import static io.restassured.RestAssured.given;
+
+public class TokenManager {
+    private static String accessToken;
+    private static Instant expiryTime;
+
+    public static String getToken() {
+        try {
+            if (accessToken == null || Instant.now().isAfter(expiryTime)) {
+                System.out.println("Renewing Token..");
+                Response response = renewToken();
+                accessToken = response.path("access_token");
+                int expiryDurationInSeconds = response.path("expires_in");
+                expiryTime = Instant.now().plusSeconds(expiryDurationInSeconds - 300);
+            } else {
+                System.out.println("Token is good to use.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("ABORT! Failed to get Token.");
+        }
+        return  "Bearer " + accessToken;
+    }
+
+    private static Response renewToken() {
+        HashMap<String, String> formParams = new HashMap<>();
+        formParams.put("grant_type", "refresh_token");
+        formParams.put("refresh_token", "AQBDEkn5lxxcPQV5oJbG8TozN-qPQjxvTit9E5PDFKnfeDhl9Gb0_ONozCy9Dir4hRhSZYWLFiI9OiOpsAoT61ZPL17LIloJOR8LMezrXkKx2m4bBu0kojKD89njpWDYaBg");
+        formParams.put("client_id", "83394bee777c45bdaead478454dce1ee");
+        formParams.put("client_secret", "3ca1ba57bd31462e9f9b2a0b1ca9abf2");
+
+        Response response = given()
+                .baseUri("https://accounts.spotify.com")
+                .formParams(formParams)
+                .contentType(ContentType.URLENC)
+                .log().all()
+                .when()
+                .post("api/token")
+                .then()
+                .spec(getResponseSpecification())
+                .extract()
+                .response();
+
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("ABORT! Renew Token Failed.");
+        }
+        return response;
+    }
+}
--- src/test/java/com/spotifyapi/api/applicationApi/PlaylistApi.java
+++ src/test/java/com/spotifyapi/api/applicationApi/PlaylistApi.java
@@ -4,11 +4,12 @@ import com.spotifyapi.api.RestResourceApi;
 import com.spotifyapi.pojo.Playlist;
 import io.restassured.response.Response;
 
+import static com.spotifyapi.api.TokenManager.getToken;
+
 public class PlaylistApi {
-    static String accessToken = "Bearer BQDVtusv5VOPd9PcB1FjUsrEuHmDhk3IXeDb9bvMKCfRW3IqWbZz3Eu-ClR5c10f1Th3AcEgf01NdbmBnPl-fVhd-rJ34DDp8C7-P2s9Vq2cyDfmd3W6paiToTSYF4dXHKQIjXAwmV0VpPqUuEBaMha7TSoU_h2hn7LNy164LFhybvgbe1dZQ2WL3zXSZ4POaCA-MIbMFB7fsw1CXTaZf0z88mjgRMhDOUc_2Jd6HGybBreLDEOy5-_qCpOw2KXhNxKmEFJQlw2m0UNIRefCOo6L2u9uXtp59oHbPlUCbqSbZpcTBsfn";
 
     public static Response post(Playlist playlist) {
-        return RestResourceApi.post(playlist, accessToken, "users/3133u3fxpnaisnp6inrt3t6fxrvm/playlists");
+        return RestResourceApi.post(playlist, getToken(), "users/3133u3fxpnaisnp6inrt3t6fxrvm/playlists");
     }
 
     public static Response post(Playlist playlist, String token) {
@@ -16,10 +17,10 @@ public class PlaylistApi {
     }
 
     public static Response get(String playlistId) {
-        return RestResourceApi.get(accessToken, "playlists/" + playlistId);
+        return RestResourceApi.get(getToken(), "playlists/" + playlistId);
     }
 
     public static Response update(Playlist playlist, String playlistId) {
-        return RestResourceApi.update(playlist, accessToken, "playlists/" + playlistId);
+        return RestResourceApi.update(playlist, getToken(), "playlists/" + playlistId);
     }
 }
