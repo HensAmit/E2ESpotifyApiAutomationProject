132ec5a58bf7870c1c9ccf58b38c796132b6b8cc
Amit Hens <hens.is.here@gmail.com>
2025-02-09 18:14:11 +0530

    lombok implemented

--- src/test/java/com/spotifyapi/tests/PlayListTests.java
+++ src/test/java/com/spotifyapi/tests/PlayListTests.java
@@ -13,12 +13,17 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PlayListTests {
 
+    public Playlist playlistBuilder(String name, String description, boolean _public) {
+        return Playlist.builder()
+                .name(name)
+                .description(description)
+                ._public(_public)
+                .build();
+    }
+
     @Test
     public void createPlaylist() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("Test Playlist 3")
-                .setDescription("My 3rd playlist")
-                .setPublic(false);
+        Playlist requestPlaylist = playlistBuilder("Test Playlist 3", "My 3rd playlist", false);
 
         Response response = PlaylistApi.post(requestPlaylist);
         assertThat(response.statusCode(), equalTo(201));
@@ -27,15 +32,12 @@ public class PlayListTests {
         Playlist responsePlaylist = response.as(Playlist.class);
         assertThat(responsePlaylist.getName(), equalTo(requestPlaylist.getName()));
         assertThat(responsePlaylist.getDescription(), equalTo(requestPlaylist.getDescription()));
-        assertThat(responsePlaylist.getPublic(), equalTo(requestPlaylist.getPublic()));
+        assertThat(responsePlaylist.get_public(), equalTo(requestPlaylist.get_public()));
     }
 
     @Test
     public void getPlaylist() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("Test Playlist 2 altered")
-                .setDescription("My 2nd playlist altered")
-                .setPublic(true);
+        Playlist requestPlaylist = playlistBuilder("Test Playlist 2 altered", "My 2nd playlist altered", true);
 
         Response response = PlaylistApi.get(DataLoader.getInstance().getPropertyData("playlist_id"));
         assertThat(response.statusCode(), equalTo(200));
@@ -44,15 +46,12 @@ public class PlayListTests {
         Playlist responsePlaylist = response.as(Playlist.class);
         assertThat(responsePlaylist.getName(), equalTo(requestPlaylist.getName()));
         assertThat(responsePlaylist.getDescription(), equalTo(requestPlaylist.getDescription()));
-        assertThat(responsePlaylist.getPublic(), equalTo(requestPlaylist.getPublic()));
+        assertThat(responsePlaylist.get_public(), equalTo(requestPlaylist.get_public()));
     }
 
     @Test
     public void updatePlaylist() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("Test Playlist 2 altered")
-                .setDescription("My 2nd playlist altered")
-                .setPublic(false);
+        Playlist requestPlaylist = playlistBuilder("Test Playlist 2 altered", "My 2nd playlist altered", false);
 
         Response response = PlaylistApi.update(requestPlaylist, DataLoader.getInstance().getPropertyData("playlist_id"));
         assertThat(response.statusCode(), equalTo(200));
@@ -60,10 +59,7 @@ public class PlayListTests {
 
     @Test
     public void shouldNotBeAbleToCreatePlaylistWithoutName() {
-        Playlist requestPlaylist = new Playlist()
-                .setName("")
-                .setDescription("Playlist Nil")
-                .setPublic(false);
+        Playlist requestPlaylist = playlistBuilder("", "Playlist Nil", false);
 
         Response response = PlaylistApi.post(requestPlaylist);
         assertThat(response.statusCode(), equalTo(400));
@@ -78,10 +74,7 @@ public class PlayListTests {
     public void shouldNotBeAbleToCreatePlaylistWithExpiredToken() {
         String invalidToken = "Bearer 12345";
 
-        Playlist requestPlaylist = new Playlist()
-                .setName("Test Playlist invalid token")
-                .setDescription("Invalid token playlist")
-                .setPublic(false);
+        Playlist requestPlaylist = playlistBuilder("Test Playlist invalid token", "Invalid token playlist", false);
 
         Response response = PlaylistApi.post(requestPlaylist, invalidToken);
         assertThat(response.statusCode(), equalTo(401));
